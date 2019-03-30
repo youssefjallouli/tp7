033eb0f584b954939c62f9916fb0bdb83faed1ba
Aerosiderolito <cesaraumon@hotmail.com>
2019-03-30 17:41:12 -0400

    Update tp{7}_Youssef_Jallouli__Cesar_Rodrguez.js

--- tp{7}_Youssef_Jallouli__Cesar_Rodrguez.js
+++ tp{7}_Youssef_Jallouli__Cesar_Rodrguez.js
@@ -8,13 +8,52 @@
  */
 
 var creerHistoire = function(n) {
-    var i = 1;
+   
+  switch (n>0){
+    case true: pause();
+      var phrase = prompt(last_3_words);
+      var tab1 = phrase.split(" ");
+      last_3_words =
+      [tab1[tab1.length - 2],tab1[tab1.length - 1],tab1[tab1.length-1]].join(" ");
+          tab2 = (tab2.concat(tab1)); 
+      confirm("Passez le contrôle à l'autre joueur!");
+ 
+      creerHistoire(n-1);
+      break;
+      
+    
+    case false :
+  print(tab1);
+      return tab1;
+      
+  }
+  return tab2
   
-    var phrase = prompt("Entrez une phrase!");
-    var tab1 = phrase.split(" ");
-    var Avant_A_derniere = ""; 
-    var Avant_derniere = "";
-    var Dernière = "";
+};
+do{var n = prompt("Vous etes combien de joueurs SVP ? ");
+}while(n<=0);
+
+var histoire = creerHistoire(n);
+print(histoire);
+
+
+
+    /* if (tab1.length>2){
+      Avant_A_derniere = tab1[tab1.length - 3];
+      Avant_derniere = tab1[tab1.length - 2];
+      Dernière = tab1[tab1.length - 1]
+    }else 
+   if (tab1.length>0) {Avant_A_derniere = tab1[0]};
+   if (tab1.length>1){Avant_derniere = tab1[1]};
+   if (tab1.length>2){Dernière = tab1[2]};
+ */
+
+  /*
+    while (i <= n) {
+    i++; 
+    var nvPhrase = prompt(three_last_words);
+    var tab2 = nvPhrase.split(" ");
+    tab1 = tab1.concat(tab2);
     if (tab1.length>2){
         Avant_A_derniere = tab1[tab1.length - 3];
         Avant_derniere = tab1[tab1.length - 2];
@@ -22,32 +61,7 @@ var creerHistoire = function(n) {
       }else 
      if (tab1.length>0) {Avant_A_derniere = tab1[0]};
      if (tab1.length>1){Avant_derniere = tab1[1]};
-     if (tab1.length>2){Dernière = tab1[2]};
-  
-  
-    var three_last_words =Avant_A_derniere + " " + Avant_derniere + " " + Dernière;
-    creerHistoire(n);
-    /*
-      while (i <= n) {
-      i++; 
-      var nvPhrase = prompt(three_last_words);
-      var tab2 = nvPhrase.split(" ");
-      tab1 = tab1.concat(tab2);
-      if (tab1.length>2){
-          Avant_A_derniere = tab1[tab1.length - 3];
-          Avant_derniere = tab1[tab1.length - 2];
-          Dernière = tab1[tab1.length - 1]
-        }else 
-       if (tab1.length>0) {Avant_A_derniere = tab1[0]};
-       if (tab1.length>1){Avant_derniere = tab1[1]};
-       if (tab1.length>2){Dernière = tab1[2]};    var three_last_words =
-        Avant_A_derniere + " " + Avant_derniere + " " + Dernière;
-      if (i < n) confirm("Passez le contrôle à l'autre joueur!");
-    }
-    return tab1.join(" ");
-  };
-  do{var n = prompt("Vous etes combien de joueurs SVP ? ");
-  }while(n<=0);
-  */
-  var histoire = creerHistoire(n);
-  print(histoire);
+     if (tab1.length>2){Dernière = tab1[2]};    var three_last_words =
+      Avant_A_derniere + " " + Avant_derniere + " " + Dernière;
+    if (i < n) confirm("Passez le contrôle à l'autre joueur!");
+   */
